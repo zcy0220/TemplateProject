fdd7eaa039b6201d675880084e63dfa61ee387ca
晨杨 <690574486@qq.com>
2022-08-12 17:48:42 +0800

    全局模块

---
+++ Assets/Scripts/GameMain/GameLauncher.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * 游戏启动项
+ */
+
+using UnityEngine;
+using GameUnityFramework;
+using GameBaseFramework.Base;
+using GameBaseFramework.Event;
+using GameUnityFramework.Resource;
+
+namespace GameMain
+{
+    public class GameLauncher : MonoBehaviour
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        /// <summary>
+        /// 开始游戏
+        /// </summary>
+        private void Start()
+        {
+            //var playerData = new PlayerData();
+            //playerData.Id = 1;
+            //playerData.TeamId = 1;
+            //playerData.UserId = 1001;
+            //playerData.Name = "测试名字A";
+            //var param = new BattleParam();
+            //param.Players.Add(playerData);
+
+            //var battle = new Battle();
+            //battle.Start(param);
+            //MonoUpdaterManager.Instance.AddFixedUpdateListener(battle.UpdateFrame);
+            G.UnityObjectManager.AsyncGameObjectInstantiate("UI/Prefabs/UIMainRoot.prefab", (obj) => { });
+        }
+    }
+}
---
+++ Assets/Scripts/GameMain/Globals/Global.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * 全局公用模块
+ */
+
+using GameBaseFramework.Event;
+using GameUnityFramework.Resource;
+using GameUnityFramework.Utils;
+
+namespace GameMain
+{
+    public class G
+    {
+        #region UnityObjectManager
+        public static UnityObjectManager _unityObjectManager;
+        public static UnityObjectManager UnityObjectManager
+        {
+            get
+            {
+                if (_unityObjectManager == null)
+                {
+                    _unityObjectManager = new("Assets/ArtPack/Pack");
+                }
+                return _unityObjectManager;
+            }
+        }
+        #endregion
+
+        #region UIEventManager
+        public static EventManager EventManager = new();
+        #endregion
+    }
+}
